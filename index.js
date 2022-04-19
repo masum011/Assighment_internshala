bed3d692bf1c8122f89eb859a37ba3bad18f2d50
ryan reza <masumre1010@gmail.com>
2022-04-19 05:39:25 +0530

    i m just cheking this command

---
+++ index.js
@@ -0,0 +1,23 @@
+
+// const a={
+//     avrage:(a,b)=>{
+//         console.log((a+b)/2);
+//     },
+//     percent:(a,b)=>{
+//         console.log((a/b)*100);
+//     }
+// }
+// a.avrage(20,10);
+// a.percent(200,100);
+// module.exports=a;
+// console.log("hello brainware university college");
+// const fs=require("fs");
+// fs.readFile("./simple.txt","utf-8",(err,data)=>{
+//     if(err){
+//         throw err;
+//     }
+//     console.log(data);
+// });
+console.log("hello node js");
+console.log("masum tuu kothai r");
+console.log(23+6);
